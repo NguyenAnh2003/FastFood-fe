01153db71352705423ba9b26fbcbd8a8e210429e
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-09-14 20:24:22 +0700

    initial commit

--- server/data/data.js
+++ server/data/data.js
@@ -10,7 +10,7 @@ const data = {
       price: 30.000,
       category: 'burger',
       isSale: false
-    }
+    }  
   ]
 }
 
--- server/models/ProductModel.js
+++ server/models/ProductModel.js
@@ -16,4 +16,4 @@ const productSchema = new mongoose.Schema(
 )
 
 const Product = mongoose.model('Product', productSchema);
-export default Product;
+export default Product;  
