8f7e157319d11a294e49cb8f2a75ebe4176c8576
HuyLe5923 <115052152+HuyLe5923@users.noreply.github.com>
2022-10-15 21:35:20 +0700

    init commit

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ import newsRouter from './routers/NewsRouter.js';
 
 // config dotenv file
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.DB_URI).then(() => {
   console.log('Connected');
 }).catch(err => console.log(err.message));
 
