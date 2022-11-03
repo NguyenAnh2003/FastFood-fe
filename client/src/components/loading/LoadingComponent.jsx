4344369264098f81fee9b864a31119780be2181d
HuyLe5923 <115052152+HuyLe5923@users.noreply.github.com>
2022-11-03 11:27:07 +0700

    Loading Component

--- client/src/components/loading/LoadingComponent.jsx
+++ client/src/components/loading/LoadingComponent.jsx
@@ -1,15 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { Dialog, Transition } from '@headlessui/react';
+import { Fragment, useState } from 'react';
 import './style.css';
 
 export default function LoadingComponent() {
+  let [isOpen, setIsOpen] = useState(true);
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
   return (
-    <div className='flex items-center justify-center h-screen bg-white'>
-      <div className="loadingContainer">
-        <div className="beakSmall"></div>
-        <div className="head"></div>
-        <div className="eye"></div>
-        <div className="beakLarge"></div>
-      </div>
-    </div>
-  )
+    <>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={closeModal}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-white" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <div className="loadingContainer">
+                  <div className="beakSmall"></div>
+                  <div className="head"></div>
+                  <div className="eye"></div>
+                  <div className="beakLarge"></div>
+                </div>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  );
 }
