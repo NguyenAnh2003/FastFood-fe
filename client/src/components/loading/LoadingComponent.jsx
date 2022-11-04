fd32289548f78b02a6994e637a16f9b907b4d52d
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-11-04 16:29:22 +0700

    fix loading

--- client/src/components/loading/LoadingComponent.jsx
+++ client/src/components/loading/LoadingComponent.jsx
@@ -21,7 +21,7 @@ export default function LoadingComponent() {
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
-            enterFrom="opacity-0"
+            enterFrom="opacity-1"
             enterTo="opacity-100"
             leave="ease-in duration-200"
             leaveFrom="opacity-100"
--- client/src/pages/favorite/CategoryDropDown.jsx
+++ client/src/pages/favorite/CategoryDropDown.jsx
@@ -38,7 +38,7 @@ export default function CategoryDropDown() {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm scrollbar-hide">
+            <Listbox.Options className="px-1.5 absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm scrollbar-hide">
               {categories.map((category, index) => (
                 <Listbox.Option
                   key={index}
