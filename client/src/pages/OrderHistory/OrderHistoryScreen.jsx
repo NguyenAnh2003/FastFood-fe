9aa33c2f110dab31bdc4f2ae12381e439017445a
HuyLe5923 <115052152+HuyLe5923@users.noreply.github.com>
2022-11-08 00:10:27 +0700

    SideBar tam thoi Huy de day nha

--- client/src/pages/OrderHistory/OrderHistoryScreen.jsx
+++ client/src/pages/OrderHistory/OrderHistoryScreen.jsx
@@ -1,68 +1,66 @@
 import React from 'react'
-import { AiOutlineHome } from 'react-icons\Ai';
+import { AiTwotoneHome } from 'react-icons/ai';
+import { MdFastfood, MdAccountCircle } from 'react-icons/md'
+import { BsNewspaper, BsBookmarkHeartFill, BsFillCartCheckFill } from 'react-icons/bs'
+import { Link } from 'react-router-dom';
 
 export default function ProfileScreen() {
   return (
     <div>
 <div class="min-h-screen bg-gray-100">
   <div class="sidebar min-h-screen w-[3.35rem] overflow-hidden border-r hover:w-56 hover:bg-white hover:shadow-lg transition-all ease-in-out">
-    <div class="flex h-screen flex-col justify-between pt-2 pb-6">
+    <div class="flex h-screen flex-col justify-between pt-2 pb-6 bg-[#c91e34]">
       <div>
-        <div class="w-max p-2.5">
-        <AiOutlineHome fill="#000" size={24} />
-        </div>
         <ul class="mt-6 space-y-2 tracking-wide">
           <li class="min-w-max">
-            <>
-            <AiOutlineHome fill="#000" size={24} />
-              <span class="-mr-1 font-medium">Dashboard</span>
-            </>
-          </li>
-          <li class="min-w-max">
-            <a href="#" class="bg group flex items-center space-x-4 rounded-full px-4 py-3 text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path class="fill-current text-gray-300 group-hover:text-cyan-300" fill-rule="evenodd" d="M2 6a2 2 0 012-2h4l2 2h4a2 2 0 012 2v1H8a3 3 0 00-3 3v1.5a1.5 1.5 0 01-3 0V6z" clip-rule="evenodd" />
-                <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M6 12a2 2 0 012-2h8a2 2 0 012 2v2a2 2 0 01-2 2H2h2a2 2 0 002-2v-2z" />
-              </svg>
-              <span class="group-hover:text-gray-700">Categories</span>
+          <a href="#" class="bg group flex items-center space-x-4 px-4 py-3 text-white hover:bg-[#f5d3d6] hover:text-[#941516] hover:scale-95 rounded-full transition duration-150 ease-in-out">
+            <AiTwotoneHome fill="#65a30d" size={20} />             
+            <span class="-mr-1 font-medium">Trang chủ</span>
             </a>
           </li>
           <li class="min-w-max">
-            <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M2 5a2 2 0 012-2h8a2 2 0 012 2v10a2 2 0 002 2H4a2 2 0 01-2-2V5zm3 1h6v4H5V6zm6 6H5v2h6v-2z" clip-rule="evenodd" />
-                <path class="fill-current text-gray-300 group-hover:text-cyan-300" d="M15 7h1a2 2 0 012 2v5.5a1.5 1.5 0 01-3 0V7z" />
-              </svg>
-              <span class="group-hover:text-gray-700">Reports</span>
+          <a href="#" class="bg group flex items-center space-x-4 px-4 py-3 text-white hover:bg-[#f5d3d6] hover:text-[#941516] hover:scale-95 rounded-full transition duration-150 ease-in-out">
+            <MdFastfood fill="#65a30d" size={20} />             
+            <span class="-mr-1 font-medium">Thực đơn</span>
             </a>
           </li>
           <li class="min-w-max">
-            <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path class="fill-current text-gray-600 group-hover:text-cyan-600" d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z" />
-                <path class="fill-current text-gray-300 group-hover:text-cyan-300" d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z" />
-              </svg>
-              <span class="group-hover:text-gray-700">Other data</span>
+          <Link to={'/news'}>
+          <a href="#" class="bg group flex items-center space-x-4 px-4 py-3 text-white hover:bg-[#f5d3d6] hover:text-[#941516] hover:scale-95 rounded-full transition duration-150 ease-in-out">
+            <BsNewspaper fill="#65a30d" size={20} />             
+            <span class="-mr-1 font-medium">Tin tức</span>
             </a>
+            </Link>
           </li>
           <li class="min-w-max">
-            <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path class="fill-current text-gray-300 group-hover:text-cyan-300" d="M4 4a2 2 0 00-2 2v1h16V6a2 2 0 00-2-2H4z" />
-                <path class="fill-current text-gray-600 group-hover:text-cyan-600" fill-rule="evenodd" d="M18 9H2v5a2 2 0 002 2h12a2 2 0 002-2V9zM4 13a1 1 0 011-1h1a1 1 0 110 2H5a1 1 0 01-1-1zm5-1a1 1 0 100 2h1a1 1 0 100-2H9z" clip-rule="evenodd" />
-              </svg>
-              <span class="group-hover:text-gray-700">Finance</span>
+          <Link to={'/saved'}>
+          <a href="#" class="bg group flex items-center space-x-4 px-4 py-3 text-white hover:bg-[#f5d3d6] hover:text-[#941516] hover:scale-95 rounded-full transition duration-150 ease-in-out">
+            <BsBookmarkHeartFill fill="#65a30d" size={20} />             
+            <span class="-mr-1 font-medium">Yêu thích</span>
             </a>
+            </Link>
+          </li>
+          <li class="min-w-max">
+          <Link to={'/cart'}>
+          <a href="#" class="bg group flex items-center space-x-4 px-4 py-3 text-white hover:bg-[#f5d3d6] hover:text-[#941516] hover:scale-95 rounded-full transition duration-150 ease-in-out">
+            <BsFillCartCheckFill fill="#65a30d" size={20} />             
+            <span class="-mr-1 font-medium">Giỏ hàng</span>
+            </a>
+            </Link>
           </li>
         </ul>
       </div>
-      <div class="w-max -mb-3">
-        <a href="#" class="group flex items-center space-x-4 rounded-md px-4 py-3 text-gray-600">
-          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 group-hover:fill-cyan-600" viewBox="0 0 20 20" fill="currentColor">
-            <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
-          </svg>
-          <span class="group-hover:text-gray-700">Settings</span>
-        </a>
+      <div >
+      <ul class="mt-6 space-y-2 tracking-wide">
+          <li class="min-w-max">
+          <Link to={'/ProfileScreen'}>
+          <a href="#" class="bg group flex items-center space-x-4 px-4 py-3 text-white hover:bg-[#f5d3d6] hover:text-[#941516] hover:scale-95 rounded-full transition duration-150 ease-in-out">
+            <MdAccountCircle fill="#65a30d" size={20} />             
+            <span class="-mr-1 font-medium">Tài khoản</span>
+            </a>
+            </Link>
+          </li>
+          </ul>
       </div>
     </div>
   </div>
