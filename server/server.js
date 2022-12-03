8f626ca781227457759425130fa448768d345da5
NguyenAnh2003 <cunho2803032003@gmail.com>
2022-12-03 19:29:12 +0700

    init commit

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ import orderRouter from './routers/OrdersRouter.js';
 
 // config dotenv file
 dotenv.config();
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+mongoose.connect(process.env.DB_URI).then(() => {
   console.log('Connected');
 }).catch(err => console.log(err.message));
 
